75ece59d3699cf04d6accb39d03e453dc3a21f03
miaoyixuan.myx <miaoyixuan.myx@alibaba-inc.com>
2025-07-07 10:36:45 +0800

    feat: log

--- src/alibabacloud_apsaradb_for_mongodb_mcp_server/server.py
+++ src/alibabacloud_apsaradb_for_mongodb_mcp_server/server.py
@@ -121,7 +121,8 @@ def get_audit_log_from_sls(
             logstore="mongo_audit_log",
             request=get_logs_from_sls_request
         )
-        return {"start_ts": {start_ts}, "end_ts": {end_ts}, "count": len(response.body), "code": response.status_code, "logs": response.body}
+        return {"start_date": {start_date}, "end_date": {end_date}, "start_ts": {start_ts}, "end_ts": {end_ts},
+                "query": {query}, "count": len(response.body), "code": response.status_code, "logs": response.body}
     except Exception as e:
         logger.error(f"Failed to get logs from sls: {str(e)}")
         return f"Failed to get logs from sls: {str(e)}"
